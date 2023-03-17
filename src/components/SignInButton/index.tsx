25619caeca86f91ecfb2ac04510cf54c64fa720b
dev-marianacavalcante <mariana.cavalcantevl@gmail.com>
2023-03-17 17:18:14 -0300

    Primeira parte do front-end e integracao com a API stripe

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi'
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
+        <button
+            className={styles.SignInButton}
+            type='button'
+        >
+            <FaGithub color='#02d361' />
+            Mariana Cavalcante
+            <FiX color='#737380' className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button
+            className={styles.SignInButton}
+            type='button'
+        >
+            <FaGithub color='#eba417' />
+            Sign in with Github
+        </button>
+    )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { Header } from '../components/Header'
+import { AppProps } from 'next/app'
+
+import '../styles/global.scss'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
+}
