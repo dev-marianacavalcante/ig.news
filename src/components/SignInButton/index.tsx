0d607fd1556855a4b4e1c20bd309cc249009262e
dev-marianacavalcante <mariana.cavalcantevl@gmail.com>
2023-03-23 21:32:00 -0300

    Autenticacao com Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession()
+    console.log(session)
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button
-            className={styles.SignInButton}
             type='button'
+            className={styles.SignInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color='#02d361' />
-            Mariana Cavalcante
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
         </button>
     ) : (
         <button
             className={styles.SignInButton}
             type='button'
+            onClick={() => signIn('github')}
         >
             <FaGithub color='#eba417' />
             Sign in with Github
